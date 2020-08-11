e834d03e8708a9e87f1146276044778630359e62
Ricardo <manosalvas2013@gmail.com>
2020-08-11 16:48:55 -0400

    controller set up

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -11,5 +11,45 @@ router.get("/", function (req, res) {
         var hbsObject = {
             burgers: data
         };
-      })
-  })
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "name", "devoured"
+    ], [req.body.name, req.body.devoured], function (result) {
+        res.json({ id: result.insertID });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+      });
+  });
+
+  router.delete("/api/burgers/:id", function (req, res) {
+      var condition = "id = " + req.params.id;
+
+      burger.delete(condition, function (req, res) {
+          if (result.affectedRows == 0) {
+              return res.status(400).end();
+          } else {
+              res.status(200).end();
+          }
+        });
+    });
+
+    module.exports = router; 
